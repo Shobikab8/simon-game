96ff657a736a2ce4c4ec346501de9ba874d21018
Shobika B <shobikab8@gmail.com>
2024-12-12 23:28:05 +0530

    added basic game logic
    checks if the complete pattern is correct

--- game-logic.js
+++ game-logic.js
@@ -9,20 +9,61 @@ const list = {
     3: yellow,
     4: blue
 };
-
-function popSquare(){
-    const num = Math.floor(Math.random()*4)+1;
-    console.log(list[num])  
-}
+let orderList = [];
+let inputList = [];
 let levelCount = 1;
-document.addEventListener("DOMContentLoaded", ()=>{
-    document.addEventListener("keydown", (e) => startGame( e, popSquare))
-})
+handleKeyPress();
 
-async function startGame(event, callback){
-    console.log("key pressed is "+event.key)
-    gameText.innerHTML = "Level "+levelCount;
+
+function handleKeyPress(){
+    document.addEventListener("DOMContentLoaded", ()=>{
+        document.addEventListener("keydown", () => startGame(popSquare));
+    })
+}
+
+function startGame(callback){
+    gameText.innerHTML = "Level 1";
     document.removeEventListener('keydown', startGame);
     callback();
 }
 
+function popSquare(){
+    gameText.innerHTML = "Level "+levelCount;
+    const num = Math.floor(Math.random()*4)+1;
+    const currentSquare = list[num];
+    orderList.push(num);
+    console.log(orderList);  
+
+    currentSquare.style.opacity = "0";
+    
+    setTimeout(() => {
+        currentSquare.style.opacity = "1";
+    }, 150);
+}
+
+function handleClick(key){
+    inputList.push(key);
+    console.log(inputList);
+    
+    if (inputList.length === orderList.length) {
+        const isCorrect = checkValue(inputList);
+        if (isCorrect) {
+            levelCount++;
+            inputList = []; 
+            popSquare(); 
+        } else {
+            gameText.innerHTML = "Game Over! Press any key to Restart";
+            orderList = [];
+            inputList = [];
+            handleKeyPress(); 
+        }
+    }
+}
+
+
+function checkValue(userInput) {
+    return userInput.length === orderList.length && 
+           userInput.every((val, index) => val === orderList[index]);
+}
+
+window.handleClick = handleClick;
